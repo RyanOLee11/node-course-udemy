4d96701088ab200c76e2b126ff0fae33fe72b5c6
ryanlee11 <rlee@insum.ca>
2019-04-11 17:55:15 -0400

    slight changes to the notes.txt file

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,3 +3,5 @@ const fs = require('fs');
 fs.writeFileSync('notes.txt', 'My name is Ryan');
 
 
+fs.appendFileSync('notes.txt', '\nThis is the second line');
+fs.appendFileSync('notes.txt', '\nThis is the second line');
