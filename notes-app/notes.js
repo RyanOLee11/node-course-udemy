115c6d535c21d2f370d68198f9320fa5027552d7
ryanlee11 <rlee@insum.ca>
2019-04-12 17:39:47 -0400

    not done the tutorial for the note taking app yet

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,34 @@
+const fs = require('fs');
+
 const notes = function(){
     return 'Your notes ...';
 };
 
-module.exports = notes;
+
+const addNotes = function(title, body){ 
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function(){
+        return notes.title === title;
+    })
+
+    if (duplicateNotes.length == 0){
+    }
+    return notes
+};
+
+
+function loadNotes(){
+
+    try{
+        var dataBuffer = fs.readFileSync(notes.json); 
+        var dataJSON = dataBuffer.toString(); 
+        return JSON.parse(dataJSON);
+    }catch{
+        return [];
+    }
+};
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addnote
+};
