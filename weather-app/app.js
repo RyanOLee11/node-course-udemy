582825ffdb40418fdbee2cab3254ed12e4db47fe
Ryan O. Lee <ryans-mbp@Ryans-MacBook-Pro.local>
2019-06-16 19:05:21 -0400

    first one back after a while just changes from before

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,47 +1,13 @@
 const request = require('request');
-
+const forecast = require("./utils/forecast.js")
 const geocode = require('./utils/geocode.js');
-// const mapBoxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/los angeles.json?access_token=pk.eyJ1Ijoicnlhbm9sZWUxMSIsImEiOiJjanVzcHE4Y2szenVmNGRvYTBsenoxZWJsIn0.dXD3f9gEZpMrkZ4ztSFcfA"
-
-// const darkSkyUrl ="https://api.darksky.net/forecast/5052b3653fee1ea15f3ea3770df03f3e/37.8267,-122.4233";
-
-
 
 geocode('Plattsburgh', (error, data)=>{
     console.log('error', error);
     console.log ('data', data );
 })
 
-
-// request({url: mapBoxUrl, json: true}, (error, response)=> {
-
-//     if (error){
-//         console.log('unable to connect to mapbox');
-//     }else if (response.body.features.length == 0){
-//         console.log('unable to find location');
-//     }
-//     else{
-//         console.log(response.body.features[0].geometry.coordinates[0]);
-//         console.log(response.body.features[0].geometry.coordinates[1]);
-//     }
-// })
-
-
-
-// request({url: darkSkyUrl, json: true}, (error, response)=>{
-//     console.log("It is currently "+response.body.currently.temperature + " degrees Farenheight. With a " + response.body.currently.precipProbability+ " chance of precipatation");
-// });
-
-
-// console.log('starting');
-
-// setTimeout(()=> {
-//     console.log('2second timer');
-// },2000)
-
-// setTimeout(()=> {
-//     console.log('timer');
-// },0)
-// console.log('stopping');
-
-
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (lat, long, callback) => {
+    const darkSkyUrl ="https://api.darksky.net/forecast/5052b3653fee1ea15f3ea3770df03f3e/" + lat + "," + long;
+    request({url: darkSkyUrl, json: true}, (error, response)=>{
+        if (error){
+            callback('unable to connect to forecast sevices');
+        }else if (response.body.length == 0 ){
+            callback('forecast was not found for the lat long given');
+        }else{
+            callback(undefined, response.body.currently);
+        }
+    })
+}
+
+module.exports = forecast;
