be422c0f7a4482f9b2903478dbf0e857578db9f8
Ryan O. Lee <ryans-mbp@Ryans-MacBook-Pro.local>
2019-06-23 22:22:14 -0400

    updated file set moved o to express

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -2,16 +2,16 @@ const request = require('request');
 
 const geocode = (address, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +".json?access_token=pk.eyJ1Ijoicnlhbm9sZWUxMSIsImEiOiJjanVzcHE4Y2szenVmNGRvYTBsenoxZWJsIn0.dXD3f9gEZpMrkZ4ztSFcfA"
-    request({url, json: true}, (error, response)=>{
+    request({url, json: true}, (error, {body})=>{
         if (error){
             callback('unable to connect to location sevices');
-        }else if (response.body.length == 0 ){
+        }else if (body.length == 0 ){
             callback('location was not found');
         }else{
             data = {
-                location: response.body.features[0].place_name,
-                long: response.body.features[0].center[1],
-                lat: response.body.features[0].center[0]
+                location: body.features[0].place_name,
+                long: body.features[0].center[1],
+                lat: body.features[0].center[0]
             }
             callback(undefined, data);
         }
