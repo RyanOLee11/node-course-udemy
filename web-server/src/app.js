33c22be8c6d596cb585c3cbc095eaa95b223fbb2
ryanlee11 <rlee@insum.ca>
2019-06-28 15:28:14 -0400

    up to styling video 2

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
-
+const hbs = require('hbs');
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
-// set application
+//define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname , '../templates/views');
+const partialsPath = path.join(__dirname , '../templates/partials');
+
+ 
+
+// setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath)
 
 // set up static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -32,6 +40,20 @@ app.get('/help', (req, res) => {
 })
 app.get('/weather', (req, res) => {
     res.send('this is the weather page');
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('error',{
+        page_type: 'Help page',
+        error_code: 404
+    });
+})
+
+app.get('*', (req,res) => {
+    res.render('error',{
+        page_type: 'Page',
+        error_code: '404'
+    });
 })
 
 app.listen(3000,() => {
