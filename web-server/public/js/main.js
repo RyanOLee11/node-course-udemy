c00c72580573e64e89f7ac9c9904b935b3659a3f
Ryan O. Lee <ryans-mbp@Ryans-MacBook-Pro.local>
2019-07-10 21:51:05 -0400

    onto to next section

--- web-server/public/js/main.js
+++ web-server/public/js/main.js
@@ -1,13 +1,27 @@
 console.log('this is loaded fromt the main js file');
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?address=!').then(response =>{
-    response.json().then(data => {
-        if(data.error){
-            console.log(data.error);
-        }
-        else{
-            console.log(data);
-        }     
+
+const message1 = document.querySelector('#message-1');
+const message2 = document.querySelector('#message-2');
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    message1.textContent = 'Loading...';
+    message2.textContent = '';
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                message1.textContent = data.error;
+            } else {
+                message1.textContent = data.location;
+                message2.textContent =  data.summary.summary;
+            }
+        })
     })
-
 })
