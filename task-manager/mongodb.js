3813344a649b1ec3cb6880a7614f305557cdffd5
Ryan O. Lee <ryans-mbp@Ryans-MacBook-Pro.local>
2019-08-19 23:17:13 -0400

    added monga

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,9 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+
+const {MongoClient, ObjectId} = require('mongodb');
+
+
 
 const connectionURL= 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
@@ -37,21 +41,39 @@ MongoClient.connect(connectionURL,{useNewUrlParser: true },(error, client) => {
     //                                     console.log(result.ops);
     //                                 })
 
-    db.collection('tasks').insertMany([
-                                            {desciption: 'Clean kitchen',
-                                            completed: true},
-                                            {desciption: 'Water flowers',
-                                            completed: true},
-                                            {desciption: 'Cook dinner',
-                                            completed: false},
-                                        ], (error, result) => {
-                                            if (error){
-                                                console.log('unable to insert');
-                                                return
-                                            }
-                                            console.log(result.ops);
-                                        })
+    // db.collection('tasks').insertMany([
+    //                                         {desciption: 'Clean kitchen',
+    //                                         completed: true},
+    //                                         {desciption: 'Water flowers',
+    //                                         completed: true},
+    //                                         {desciption: 'Cook dinner',
+    //                                         completed: false},
+    //                                     ], (error, result) => {
+    //                                         if (error){
+    //                                             console.log('unable to insert');
+    //                                             return
+    //                                         }
+    //                                         console.log(result.ops);
+    //                                     })
+    
     
 
+    db.collection('tasks').findOne({_id: new ObjectId("5d4e2aa5fda48a0478af9c40")},(error, user) => {
+        if(error){
+            console.log('there was an error finding a user');
+            return;
+        }
+
+        console.log(user);
+    })
+
+    db.collection('tasks').find({completed: false}).toArray((error, users) => {
+        if(error){
+            console.log('there was an error finding a user');
+            return;
+        }
+
+        console.log(users);
+    })
 
 })
